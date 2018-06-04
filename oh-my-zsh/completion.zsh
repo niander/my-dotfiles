ba92a915d0335e852ac699677fa47b9306476b67
niander <niander@users.noreply.github.com>
2018-06-04 09:02:01 -0300

    added new omz theme and plugins

--- oh-my-zsh/completion.zsh
+++ oh-my-zsh/completion.zsh
@@ -7,7 +7,7 @@ export ZSH=/home/niander/.oh-my-zsh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="maran"   #"robbyrussell"
+ZSH_THEME="robbyrussell" #"maran"
 DEFAULT_USER="niander"
 
 # Uncomment the following line to use case-sensitive completion.
@@ -52,7 +52,7 @@ ZSH_CUSTOM=$DOTFILES/oh-my-zsh/custom
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git tmux globalias sudo dircycle changes)
+plugins=(git tmux globalias sudo dircycle wd change) #themes
 
 # tmux plugin
 ZSH_TMUX_AUTOSTART=false
