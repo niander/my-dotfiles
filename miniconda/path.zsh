b6eb623f576727a4fef42d599f0ed19404db0450
niander <niander@users.noreply.github.com>
2018-06-04 16:50:17 -0300

    miniconda new sourcing script

--- miniconda/path.zsh
+++ miniconda/path.zsh
@@ -2,4 +2,4 @@
 [[ -d $HOME/miniconda2/bin ]] && export PATH="$HOME/miniconda2/bin:$PATH" && exit
 
 # added by Miniconda3 installer
-[[ -d $HOME/miniconda3/bin ]] && export PATH="$HOME/miniconda3/bin:$PATH"
+[[ -d $HOME/miniconda3/bin ]] && source $HOME/miniconda3/etc/profile.d/conda.sh # export PATH="$HOME/miniconda3/bin:$PATH"
