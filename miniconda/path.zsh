63be4295b0696493f852506722c12b3d2e1bbeff
niander <niander@users.noreply.github.com>
2018-06-06 10:47:09 -0300

    fix conda and tmux

--- miniconda/path.zsh
+++ miniconda/path.zsh
@@ -1,5 +1,5 @@
 # added by Miniconda2 4.0.5 installer
-[[ -d $HOME/miniconda2/bin ]] && export PATH="$HOME/miniconda2/bin:$PATH" && exit
+[[ -d $HOME/miniconda2/bin ]] && export PATH="$HOME/miniconda2/bin:$PATH" && return
 
 # added by Miniconda3 installer
 [[ -d $HOME/miniconda3/bin ]] && source $HOME/miniconda3/etc/profile.d/conda.sh # export PATH="$HOME/miniconda3/bin:$PATH"
