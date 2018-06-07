8d3c9bda52a299425cab1ab8262667891a8b398b
niander <niander@users.noreply.github.com>
2018-06-06 21:07:30 -0300

    fixed all installs

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,8 +1,13 @@
-   
-#install oh my zsh
-#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+#!/bin/sh
 
-# Make zsh default shell
-which zsh > /dev/null 2>&1 && sudo chsh -s $(which zsh)
+# install oh my zsh
+if ! test -d "$HOME/.oh-my-zsh"
+then
+  wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+  
+  # Make zsh default shell
+  which zsh >/dev/null 2>&1 && sudo chsh -s `which zsh`
+fi
+
+
 
