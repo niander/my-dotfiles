e221f41724fd06227081eda9962845f91da0d3aa
niander <niander@users.noreply.github.com>
2018-06-14 20:26:31 -0300

    base16-shell: changed installation dir

--- base16-shell/config.zsh
+++ base16-shell/config.zsh
@@ -1,3 +1,3 @@
-BASE16_SHELL=$HOME/.config/base16-shell/
+BASE16_SHELL=$DOTFILES/base16-shell/.base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 [ -z "$BASE16_THEME" ] && base16_eighties
--- base16-shell/install.sh
+++ base16-shell/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASE16_SHELL=$HOME/.config/base16-shell/
+BASE16_SHELL=$DOTFILES/base16-shell/.base16-shell/
 if ! test -d $BASE16_SHELL
 then
   git clone https://github.com/chriskempson/base16-shell.git $BASE16_SHELL
