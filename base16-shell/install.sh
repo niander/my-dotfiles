80de411112a1dc52f94abae7fd69e1bee879039e
niander <niander@users.noreply.github.com>
2018-06-14 09:12:31 -0300

    base16: added update option in install

--- base16-shell/install.sh
+++ base16-shell/install.sh
@@ -4,4 +4,7 @@ BASE16_SHELL=$HOME/.config/base16-shell/
 if ! test -d $BASE16_SHELL
 then
   git clone https://github.com/chriskempson/base16-shell.git $BASE16_SHELL
+else
+  cd $BASE16_SHELL
+  git pull --rebase --stat origin master
 fi
