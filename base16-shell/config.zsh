b5f2295f066a01bcd2226d6a8d07f304c71b2b85
niander <niander@users.noreply.github.com>
2018-06-26 01:00:50 -0300

    base16: only load in emulated terminals

--- base16-shell/config.zsh
+++ base16-shell/config.zsh
@@ -1,3 +1,5 @@
 BASE16_SHELL=$DOTFILES/base16-shell/.base16-shell/
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
-[ -z "$BASE16_THEME" ] && base16_eighties
+if ! [[ $TTY =~ "tty" ]]; then
+    [[ -n "$PS1" ]] && [[ -s $BASE16_SHELL/profile_helper.sh ]] && eval "$($BASE16_SHELL/profile_helper.sh)"
+    [[ -z "$BASE16_THEME" ]] && base16_eighties
+fi
