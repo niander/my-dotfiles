ae0f2dcd7812ad6c11873048d634e18b9d5eab57
niander <niander@users.noreply.github.com>
2018-06-26 01:03:33 -0300

    conda: miniconda3 as first

--- miniconda/path.zsh
+++ miniconda/path.zsh
@@ -1,5 +1,6 @@
-# added by Miniconda2 4.0.5 installer
-[[ -d $HOME/miniconda2/bin ]] && export PATH="$HOME/miniconda2/bin:$PATH" && return
-
 # added by Miniconda3 installer
-[[ -d $HOME/miniconda3/bin ]] && . $HOME/miniconda3/etc/profile.d/conda.sh && conda activate
+[[ -d $HOME/miniconda3/bin ]] && source $HOME/miniconda3/etc/profile.d/conda.sh && conda activate; return
+
+# added by Miniconda2 4.0.5 installer
+[[ -d $HOME/miniconda2/bin ]] && export PATH="$HOME/miniconda2/bin:$PATH"
+
