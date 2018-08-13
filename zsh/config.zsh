445817a51574f662775fb8eca78f39ab040187f0
niander <niander@users.noreply.github.com>
2018-08-13 09:36:55 -0300

    zsh: updated config file

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,37 +1,36 @@
-#export LSCOLORS="exfxcxdxbxegedabagacad"
-#export CLICOLOR=true
+## Colors
+# ls colors on mac
+[[ -z "$LSCOLORS" ]] && export LSCOLORS="exfxcxdxbxegedabagacad"
+[[ -z "$CLICOLOR" ]] && export CLICOLOR=true
 
+## Functions
 fpath=($DOTFILES/functions $fpath)
-
 autoload -U $DOTFILES/functions/*(:t)
 
+## History
 [[ -z "$HISTFILE" ]] && HISTFILE="$HOME/.zsh_history"
 [[ -z "$HISTSIZE" ]] && HISTSIZE=50000
 [[ -z "$SAVEHIST" ]] && SAVEHIST=10000
 
+## Options
 # don't nice background tasks
 setopt NO_BG_NICE
-#setopt NO_HUP
 setopt NO_LIST_BEEP
 
 # expand history before executing
 setopt HIST_VERIFY
 # add timestamps to history
 setopt EXTENDED_HISTORY
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-
 # adds history incrementally and share it across sessions
 setopt SHARE_HISTORY
-
+setopt CORRECT
+setopt PROMPT_SUBST
+setopt COMPLETE_IN_WORD
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
 
-#bindkey '^[^[[D' backward-word
-#bindkey '^[^[[C' forward-word
-#bindkey '^[[5D' beginning-of-line
-#bindkey '^[[5C' end-of-line
-#bindkey '^[[3~' delete-char
-#bindkey '^?' backward-delete-char
+## Key bindings
+autoload -U insert-files
+zle -N insert-files
+bindkey '^Xf' insert-files
