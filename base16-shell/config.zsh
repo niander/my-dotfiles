f9bd7715c759dae8ec6ef347013502b6c71a6087
niander <niander@users.noreply.github.com>
2019-03-11 09:36:18 -0700

    base16: fixed wrong path

--- base16-shell/config.zsh
+++ base16-shell/config.zsh
@@ -1,4 +1,4 @@
-BASE16_SHELL=$DOTFILES/base16-shell/.base16-shell/
+BASE16_SHELL=$DOTFILES/base16-shell/.base16-shell
 if ! [[ $TTY =~ "tty" ]]; then
     [[ -n "$PS1" ]] && [[ -s $BASE16_SHELL/profile_helper.sh ]] && eval "$($BASE16_SHELL/profile_helper.sh)"
     [[ -z "$BASE16_THEME" ]] && base16_eighties
