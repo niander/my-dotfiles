fab323b38c97e9c54da135ddbe71fcedc23ee91f
niander <niander@users.noreply.github.com>
2020-10-19 17:17:15 -0700

    base16: fix the use in WSL with windows terminal

--- base16-shell/config.zsh
+++ base16-shell/config.zsh
@@ -1,5 +1,5 @@
 BASE16_SHELL=$DOTFILES/base16-shell/.base16-shell
-if ! [[ $TTY =~ "tty" ]]; then
+if [[ ! $TTY =~ "tty" || ! -z $WSL_DISTRO_NAME ]]; then
     [[ -n "$PS1" ]] && [[ -s $BASE16_SHELL/profile_helper.sh ]] && eval "$($BASE16_SHELL/profile_helper.sh)"
     [[ -z "$BASE16_THEME" ]] && base16_eighties
 fi
