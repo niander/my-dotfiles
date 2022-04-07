c66528a431c5ce3d90efe31e0bc71119feea7838
niander <niander@users.noreply.github.com>
2022-04-07 10:56:51 -0700

    git: remove unnecessary aliases and fix one

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,6 @@
-alias gl='git pull --prune'
-alias gp='git push origin HEAD'
-
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 #alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gcpb='git copy-branch-name'
-alias gac='git add -all && git commit -m'
+alias gac='git add --all && git commit -m'
 alias ge='git-edit-new'
