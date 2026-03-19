a2a778b2ce504af694d38520b4ae853a414f2808
niander <niander@users.noreply.github.com>
2026-03-19 19:05:11 +0000

    base16: alias was being resolved in the same line as definition

--- base16-shell/config.zsh
+++ base16-shell/config.zsh
@@ -1,5 +1,7 @@
 BASE16_SHELL=$DOTFILES/base16-shell/.base16-shell
-if [[ -z $TERM_PROGRAM && (! $TTY =~ "tty" || ! -z $WSL_DISTRO_NAME) ]]; then
+should_apply_base16=false
+if [[ -z $TERM_PROGRAM && (! $TTY =~ "tty" || -n $WSL_DISTRO_NAME) ]]; then
+    should_apply_base16=true
     [[ -n "$PS1" ]] && [[ -s $BASE16_SHELL/profile_helper.sh ]] && source "$BASE16_SHELL/profile_helper.sh"
-    [[ -z "$BASE16_THEME" ]] && base16_eighties
 fi
+$should_apply_base16 && [[ -z "$BASE16_THEME" ]] && base16_eighties
