02ec47e5eb1b433a0cbc089eefa00733456d7b40
niander <niander@users.noreply.github.com>
2026-05-20 14:12:14 -0700

    intel: add intel oneapi source path

---
+++ intel/path.zsh
@@ -0,0 +1,5 @@
+# [[ -d /opt/intel/oneapi/ ]] && source /opt/intel/oneapi/compiler/2025.0/env/vars.sh
+
+# [[ -d /opt/intel/oneapi/ ]] && source /opt/intel/oneapi/umf/0.9/env/vars.sh
+
+# [[ -d /opt/intel/oneapi/ ]] && source /opt/intel/oneapi/pti/0.10/env/vars.sh
