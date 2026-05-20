877e2ca278916cfe0ab19ad8926e3898253280bc
niander <niander@users.noreply.github.com>
2026-05-20 14:19:07 -0700

    path: add ~/.local/bin to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,4 @@
 #export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH="$DOTFILES/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 #export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
