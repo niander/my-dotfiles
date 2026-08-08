7d05ebab5df92579dc36ec98c65e7c6da21f26d2
niander <4544122+niander@users.noreply.github.com>
2026-08-08 15:37:31 -0700

    miniconda: fix unreachable guard, drop miniconda2

--- miniconda/path.zsh
+++ miniconda/path.zsh
@@ -1,6 +1,4 @@
-# added by Miniconda3 installer
-[[ -d $HOME/miniconda3/bin ]] && source $HOME/miniconda3/etc/profile.d/conda.sh && conda activate; return
-
-# added by Miniconda2 4.0.5 installer
-[[ -d $HOME/miniconda2/bin ]] && export PATH="$HOME/miniconda2/bin:$PATH"
-
+if [[ -d $HOME/miniconda3/bin ]]
+then
+  source $HOME/miniconda3/etc/profile.d/conda.sh && conda activate
+fi
