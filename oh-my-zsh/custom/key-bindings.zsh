bcf0c52c1328a24fdbc16fc5150bc33fe1f11614
niander <4544122+niander@users.noreply.github.com>
2026-08-09 22:22:45 -0700

    omz: rebind esc esc to clear the line

--- oh-my-zsh/custom/key-bindings.zsh
+++ oh-my-zsh/custom/key-bindings.zsh
@@ -5,3 +5,11 @@ fi
 if [[ "${terminfo[knp]}" != "" ]]; then
     bindkey -r "${terminfo[knp]}"
 fi
+
+# [Esc] [Esc] clears the whole buffer (yank it back with ^Y).
+# The sudo plugin claims that chord for prefix-with-sudo; move it to [Alt] [s].
+for keymap in emacs vicmd viins; do
+    bindkey -M $keymap '\e\e' kill-buffer
+    (( $+widgets[sudo-command-line] )) && bindkey -M $keymap '\es' sudo-command-line
+done
+unset keymap
